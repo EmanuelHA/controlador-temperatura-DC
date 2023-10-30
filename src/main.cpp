222d15866e5911307c67bcfa652c901a460e74ff
EmanuelHA <emanuel.hernandezarauz@ucr.ac.cr>
2023-10-29 22:23:27 -0600

    Soporte agregado para OLED SSD1306

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include <Arduino.h>
+#include <TinyI2CMaster.h>
+#define  TINY4KOLED_QUICK_BEGIN // Optimizacion de espacio en OLED blanca
+#include <Tiny4kOLED.h>
+#include "ModernDos8.h"
 #include <PID_v1.h>
 
 #if defined(__AVR_ATtiny85__)
@@ -21,33 +25,21 @@ const float R1 = 100000;
 const float A = 2.114990448e-03, B = 0.3832381228e-04, C = 5.228061052e-07;
 // Declaracion del controlador PID
 PID PIDcontroller(&input, &output, &setpoint, kp, ki, kd, REVERSE);
-// Declaracion de prototipos de fucion
-float ln(float);
-float readTemperature();
 
-void setup() {
-  // Inicializacion del control PID
-  setpoint = 220.0f;
-  input = readTemperature();
-  PIDcontroller.SetMode(AUTOMATIC);
-}
-
-void loop() {
-  PIDcontroller.Compute();
-  analogWrite(PIN_MOSFET, output);
-  input = readTemperature();
-  delay(100);
-}
-
-float ln (float x) {
+float ln(float x) { // LingDong- / ln.c -> -212 bytes vs. log()
   unsigned int bx = * (unsigned int *) (&x);
   unsigned int ex = bx >> 23;
   signed int t = (signed int)ex-(signed int)127;
   unsigned int s = (t < 0) ? (-t) : t;
   bx = 1065353216 | (bx & 8388607);
   x = * (float *) (&bx);
-  return -1.7417939+(2.8212026+(-1.4699568+(0.44717955-0.056570851*x)*x)*x)*x
-+0.6931471806*t;
+  return
+  /* less accurate */
+  //  -1.49278+(2.11263+(-0.729104+0.10969*x)*x)*x 
+  /* OR more accurate */      
+  -1.7417939 + (2.8212026 + (-1.4699568 + (0.44717955-0.056570851 * x) * x) * x) * x
+  /* compensate for the ln(2)s. ln(2)=0.6931471806 */    
+    + 0.6931471806*t;
 }
 
 float readTemperature() {
@@ -60,4 +52,33 @@ float readTemperature() {
   temperature = (1.0 / (A + lnR2*(B + C*lnR2*lnR2)));
   // Conversion y retorno de grados Kelvin a Celsius
   return (temperature - 273.15);
+}
+
+void updateDisplay() {
+  oled.clear();
+  oled.setCursor(0,1);
+  oled.print(F("TEMPERATURE: "));
+  oled.print(input);
+  oled.print(F(" °C"));
+  oled.switchFrame();
+}
+
+void setup() {
+  // Inicializacion OLED
+  oled.begin();
+  oled.setFont(FONT8X8MDOS);
+  oled.on();
+  oled.switchRenderFrame();
+  // Inicializacion del control PID
+  setpoint = 240.0f;
+  input = readTemperature();
+  PIDcontroller.SetMode(AUTOMATIC);
+}
+
+void loop() {
+  PIDcontroller.Compute();
+  analogWrite(PIN_MOSFET, output);
+  input = readTemperature();
+  updateDisplay();
+  delay(500);
 }
